dda0b6243c7a858ce9a2ba6fe9098812483784a5
MarvellousIsaiah <isaiahugoeze@gmail.com>
2024-09-03 10:07:33 +0100

    first commit

---
+++ src/main/java/com/example/devoppss/DevOppssApplication.java
@@ -0,0 +1,13 @@
+package com.example.devoppss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOppssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOppssApplication.class, args);
+    }
+
+}
